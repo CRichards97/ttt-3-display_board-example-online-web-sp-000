a195430847a3c70fe8939e6f8d1e7589803a42ac
Clint Richards <richardsclint17@gmail.com>
2020-07-23 18:25:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 def display_board
   puts "a tic tac toe board"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
 end
 
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+
